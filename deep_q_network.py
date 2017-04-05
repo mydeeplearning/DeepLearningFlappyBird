df5fdec2eac0671cfe63db30ffb5f2d7b71e9132
yinchuandong <johncdyin@gmail.com>
2017-04-05 22:22:01 +1000

    update: good upgrade 1

--- deep_q_network.py
+++ deep_q_network.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import tensorflow as tf
 import cv2
 import sys
+import os
 sys.path.append("game/")
 import wrapped_flappy_bird as game
 import random
@@ -19,7 +20,6 @@ FINAL_EPSILON = 0.0001  # final value of epsilon
 INITIAL_EPSILON = 0.0001  # starting value of epsilon
 REPLAY_MEMORY = 50000  # number of previous transitions to remember
 BATCH = 32  # size of minibatch
-FRAME_PER_ACTION = 1
 
 
 def weight_variable(shape):
@@ -90,7 +90,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
 
     # open up a game state to communicate with emulator
-    game_state = game.GameState()
+    game_state = game.FlappyBird()
 
     # store the previous observations in replay memory
     D = deque()
@@ -103,8 +103,6 @@ def trainNetwork(s, readout, h_fc1, sess):
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
     x_t, r_0, terminal = game_state.frame_step(do_nothing)
-    x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
-    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
 
     # saving and loading networks
@@ -125,28 +123,23 @@ def trainNetwork(s, readout, h_fc1, sess):
         readout_t = readout.eval(feed_dict={s: [s_t]})[0]
         a_t = np.zeros([ACTIONS])
         action_index = 0
-        if t % FRAME_PER_ACTION == 0:
-            if random.random() <= epsilon:
-                print("----------Random Action----------")
-                action_index = random.randrange(ACTIONS)
-                a_t[random.randrange(ACTIONS)] = 1
-            else:
-                action_index = np.argmax(readout_t)
-                a_t[action_index] = 1
+        if random.random() <= epsilon:
+            print("----------Random Action----------")
+            action_index = random.randrange(ACTIONS)
+            a_t[random.randrange(ACTIONS)] = 1
         else:
-            a_t[0] = 1  # do nothing
+            action_index = np.argmax(readout_t)
+            a_t[action_index] = 1
 
         # scale down epsilon
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
         # run the selected action and observe next state and reward
-        x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
-        x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
-        ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
+        x_t1, r_t, terminal = game_state.frame_step(a_t)
         x_t1 = np.reshape(x_t1, (80, 80, 1))
         #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
-        s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
+        s_t1 = np.append(s_t[:, :, 1:], x_t1, axis=2)
 
         # store the transition in D
         D.append((s_t, a_t, r_t, s_t1, terminal))
@@ -187,6 +180,8 @@ def trainNetwork(s, readout, h_fc1, sess):
 
         # save progress every 10000 iterations
         if t % 10000 == 0:
+            if not os.path.exists('saved_networks'):
+                os.mkdir('saved_networks')
             saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
 
         # print info
